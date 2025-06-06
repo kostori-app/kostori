e0bc918eb40146a733ec182fab67f786620c910b
AnxuNA <41771421+axlmly@users.noreply.github.com>
2025-06-07 04:52:17 +0800

    fix

--- lib/components/bangumi_widget.dart
+++ lib/components/bangumi_widget.dart
@@ -2,7 +2,6 @@
 
 import 'dart:io';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
