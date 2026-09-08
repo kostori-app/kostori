80d448225181d963b40186e553f30d6a56cf837e
AnxuNA <41771421+axlmly@users.noreply.github.com>
2026-09-08 08:51:53 +0800

    revert spacing; prepare subpixel hairline fix

--- lib/pages/me/me_plugins_shell.dart
+++ lib/pages/me/me_plugins_shell.dart
@@ -279,7 +279,7 @@ class _PluginShellPageState extends State<PluginShellPage>
             children: [
               if (_nav.length > 1)
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(12, 4, 12, 0),
+                  padding: const EdgeInsets.fromLTRB(12, 4, 12, 2),
                   child: Align(
                     alignment: Alignment.centerLeft,
                     child: _CapsuleBar(
